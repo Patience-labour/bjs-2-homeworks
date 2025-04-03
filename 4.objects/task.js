9c5b5318fe41953922eba31a5e459d886d1d0e81
Maxon <zheka.kartov@mail.ru>
2025-04-03 11:25:30 +0300

    Edit code

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -11,18 +11,19 @@ Student.prototype.setSubject = function (subjectName) {
 }
 
 Student.prototype.addMarks = function (...marks) {
-    if (this.marks.length === 0) {
-        console.log('Вы отчислены')
+    if (this.hasOwnProperty('marks')) {
+        this.marks.push(...marks);
     }
-    this.marks = marks;
+
 }
 
 Student.prototype.getAverage = function () {
-    if (this.marks.length === 0) {
+    if (this.hasOwnProperty('marks') && this.marks.length !== 0) {
+        let sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+        return sum / this.marks.length;
+    } else {
         return 0;
     }
-    let sum = this.marks.reduce((acc, mark) => acc + mark, 0)
-    return sum / this.marks.length
 }
 
 Student.prototype.exclude = function (reason) {
@@ -30,3 +31,15 @@ Student.prototype.exclude = function (reason) {
     delete this.marks;
     this.exclude = reason;
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+console.log(student1.getAverage()); // 0
+student1.addMarks(4, 5, 4, 5);
+console.log(student1.getAverage()); // 4.5
+console.log(student1);
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба')
+console.log(student2)
